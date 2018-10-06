216f18d4e2e5b9e0fc25332c1977b9ce665a79d5
Kary Huang <30398031+karyhuang@users.noreply.github.com>
2018-10-06 18:53:23 -0400

    Adding another line as per #3

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ console.log('hello');
 console.log('this line is added by test1 branch');
 
 console.log('this line is added by Kary Test after forking');
+
+console.log('this 4th line is suggested by an issue');
