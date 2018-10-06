b130264d36d5c4601cebd1240f32430971435e91
Kary Test <43917431+karytest@users.noreply.github.com>
2018-10-06 19:02:10 -0400

    Added star as per #4

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ console.log('this line is added by test1 branch');
 console.log('this line is added by Kary Test after forking');
 
 console.log('this 4th line is suggested by an issue, issue addressed');
+
+console.log('STAR!!!');
